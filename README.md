7565606f8330e692577188f962836f572f667bfa
rhn014 <Roland.Hyden@RegionHalland.se>
2019-08-22 10:11:03 +0200

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,129 @@
-# region-halland-acf-front-page-blurbs
-Funktion för att skapa blurbar på sajtens framsida
+# Front-page-plugin för ACF-blurbar på sajtens front-page
+
+## Hur man använder Region Hallands plugin "region-halland-acf-front-page-blurbs"
+
+Nedan följer instruktioner hur du kan använda pluginet "region-halland-acf-front-page-blurbs".
+
+
+## Användningsområde
+
+Denna plugin skapar en array() med med blurbar
+
+
+## Installation och aktivering
+
+```sh
+A) Hämta pluginen via Git eller läs in det med Composer
+B) Installera Region Hallands plugin i Wordpress plugin folder
+C) Aktivera pluginet inifrån Wordpress admin
+```
+
+
+## Hämta hem pluginet via Git
+
+```sh
+git clone https://github.com/RegionHalland/region-halland-acf-front-page-blurbs.git
+```
+
+
+## Läs in pluginen via composer
+
+Dessa två delar behöver du lägga in i din composer-fil
+
+Repositories = var pluginen är lagrad, i detta fall på github
+
+```sh
+"repositories": [
+  {
+    "type": "vcs",
+    "url": "https://github.com/RegionHalland/region-halland-acf-front-page-blurbs.git"
+  },
+],
+```
+Require = anger vilken version av pluginen du vill använda, i detta fall version 1.0.0
+
+OBS! Justera så att du hämtar aktuell version.
+
+```sh
+"require": {
+  "regionhalland/region-halland-acf-front-page-blurbs": "1.0.0"
+},
+```
+
+
+## Loopa ut blurbarna via "Blade"
+
+```sh
+@php($myFrontPageBlurbs = get_region_halland_acf_front_page_blurbs())
+@if(isset($myFrontPageBlurbs))
+<div>
+    <ul>
+    @foreach ($myFrontPageBlurbs as $blurbs)
+        <li>
+            <div class="rh-blurb mx1 my2">
+                <div>
+                    <img src="{!! $blurbs['image_url'] !!}" alt="{{$blurbs['image_alt']}}">
+                </div>
+                <div>
+                    <h3><a href="{{ $blurbs['link_url'] }}" target="{{ $blurbs['link_target'] }}">{{ $blurbs['link_title'] }}</a></h3>
+                    <p>{{ $blurbs['text_content'] }}</p>
+                </div>
+            </div>
+        </li>
+    @endforeach
+    </ul>
+</div>
+@endif
+```
+
+
+## Exempel på hur arrayen kan se ut
+
+```sh
+array (size=4)
+  0 => 
+    array (size=8)
+      'link_url' => string 'https://exempelsajt-A' (length=21)
+      'link_title' => string 'Exempelsajt A' (length=13)
+      'link_target' => string '' (length=0)
+      'text_content' => string 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean id diam in erat egestas vehicula eu accumsan ligula.' (length=116)
+      'image_url' => string 'https://exempel.se/app/uploads/2019/08/nyhet_1.jpg' (length=50)
+      'image_alt' => string '' (length=0)
+      'image_width' => int 400
+      'image_height' => int 180
+  1 => 
+    array (size=8)
+      'link_url' => string 'https://exempelsajt-B' (length=21)
+      'link_title' => string 'Exempelsajt B' (length=13)
+      'link_target' => string '' (length=0)
+      'text_content' => string 'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae.' (length=87)
+      'image_url' => string 'https://exempel.se/app/uploads/2019/08/nyhet_2.jpg' (length=50)
+      'image_alt' => string '' (length=0)
+      'image_width' => int 400
+      'image_height' => int 180
+  2 => 
+    array (size=8)
+      'link_url' => string 'https://exempelsajt-C' (length=21)
+      'link_title' => string 'Exempelsajt C' (length=13)
+      'link_target' => string '' (length=0)
+      'text_content' => string 'Aldu integer id metus pellentesque, suscipit mauris vel, placerat purus. Vestibulum diam elit.' (length=94)
+      'image_url' => string 'https://exempel.se/app/uploads/2019/08/nyhet_3.jpg' (length=50)
+      'image_alt' => string '' (length=0)
+      'image_width' => int 400
+      'image_height' => int 180
+  3 => 
+    array (size=8)
+      'link_url' => string 'https://exempelsajt-D' (length=21)
+      'link_title' => string 'Exempelsajt D' (length=13)
+      'link_target' => string '' (length=0)
+      'text_content' => string 'Mauris id consectetur lorem. Cras volutpat massa leo. Sed a enim ultricies, dapibus ex nec.' (length=91)
+      'image_url' => string 'https://exempel.se/app/uploads/2019/08/nyhet_1.jpg' (length=50)
+      'image_alt' => string '' (length=0)
+      'image_width' => int 400
+      'image_height' => int 180
+```
+
+## Versionhistorik
+
+### 1.0.0
+- Första version
---
+++ region-halland-acf-front-page-blurbs.php
@@ -0,0 +1,190 @@
+<?php
+
+	/**
+	 * @package Region Halland ACF Front Page Blurbs
+	 */
+	/*
+	Plugin Name: Region Halland ACF Front Page Blurbs
+	Description: Front-page-plugin för ACF-blurbar på sajtens front-page
+	Version: 1.0.0
+	Author: Roland Hydén
+	License: GPL-3.0
+	Text Domain: regionhalland
+	*/
+
+	// Anropa funktion om ACF är installerad & aktiverad
+	add_action('acf/init', 'my_acf_add_front_page_blurbs_field_groups');
+
+	// Lägg till formulär så att det går att skapa länkar
+	function my_acf_add_front_page_blurbs_field_groups() {
+
+		if (function_exists('acf_add_local_field_group')) {
+		    acf_add_local_field_group(array(
+		    'key' => 'group_1000252',
+		    'title' => __('Startsida', 'halland'),
+		    'fields' => array(
+		        0 => array(
+		            'key' => 'field_1000253',
+		            'label' => __('Puffar', 'halland'),
+		            'name' => 'name_1000254',
+		            'type' => 'repeater',
+		            'instructions' => __('Klicka på "Lägg till rad" för att lägga till en länk. Maximalt 4 stycken.', 'halland'),
+		            'required' => 0,
+		            'conditional_logic' => 0,
+		            'wrapper' => array(
+		                'width' => '',
+		                'class' => '',
+		                'id' => '',
+		            ),
+		            'collapsed' => '',
+		            'min' => 0,
+		            'max' => 4,
+		            'layout' => 'block',
+		            'button_label' => '',
+		            'sub_fields' => array(
+		                0 => array(
+		                    'key' => 'field_1000255',
+		                    'label' => __('Länk', 'regionhalland'),
+		                    'name' => 'name_1000256',
+		                    'type' => 'link',
+		                    'instructions' => __('Länka till valfri sida eller webbplats.', 'regionhalland'),
+		                    'required' => 0,
+		                    'conditional_logic' => 0,
+		                    'wrapper' => array(
+		                        'width' => '',
+		                        'class' => '',
+		                        'id' => '',
+		                    ),
+		                    'return_format' => 'array',
+		                ),
+		                1 => array(
+		                    'key' => 'field_1000257',
+						    'label' => 'Text',
+						    'name' => 'name_1000258',
+						    'type' => 'textarea',
+						    'instructions' => 'Kort text (max 200 tecken)',
+						    'required' => 0,
+						    'conditional_logic' => 0,
+						    'wrapper' => [
+						        'width' => '',
+						        'class' => '',
+						        'id' => '',
+						    ],
+						    'default_value' => '',
+						    'placeholder' => '',
+						    'maxlength' => '200',
+						    'rows' => '2',
+						    'new_lines' => '',
+		                ),
+				        2 => array(
+				        	'key' => 'field_1000259',
+						    'label' => 'Illustrativ bild',
+						    'name' => 'name_1000260',
+						    'type' => 'image',
+						    'instructions' => '',
+						    'required' => 0,
+						    'conditional_logic' => 0,
+						    'wrapper' => [
+						        'width' => '',
+						        'class' => '',
+						        'id' => '',
+						    ],
+						    'return_format' => 'array',
+						    'preview_size' => 'thumbnail',
+						    'library' => 'all',
+						    'min_width' => '',
+						    'min_height' => '',
+						    'min_size' => '',
+						    'max_width' => '',
+						    'max_height' => '',
+						    'max_size' => '',
+						    'mime_types' => '',
+				        ),
+		            ),
+		        ),
+		    ),
+		    'location' => array(
+		        0 => array(
+		            0 => array(
+		                'param' => 'page_type',
+		                'operator' => '==',
+		                'value' => 'front_page',
+		            ),
+		        ),
+		    ),
+		    'menu_order' => 0,
+		    'position' => 'normal',
+		    'style' => 'default',
+		    'label_placement' => 'top',
+		    'instruction_placement' => 'label',
+		    'hide_on_screen' => '',
+		    'active' => 1,
+		    'description' => 'Används för att skapa innehåll specifikt för startsidan.',
+		));
+		}
+
+	}
+
+	// Returnera inlagda länkar
+	function get_region_halland_acf_front_page_blurbs() {
+		
+		// Hämta id för front-page
+		$myFrontPageID = get_option('page_on_front');
+		
+		// Hämta alla inlagda länkar
+		$myBlurbs = get_field('name_1000254', $myFrontPageID);
+
+		// Skapa array med länkar
+        $myData = array();
+        
+        // Loopa igenom allt 
+        foreach ($myBlurbs as $blurbs) {
+	        
+        	// Länkar
+	        $strLinkUrl	= $blurbs['name_1000256']['url'];
+	        $strLinkTitle = $blurbs['name_1000256']['title'];
+	        $strLinkTarget = $blurbs['name_1000256']['target'];
+	        
+	        // Text
+	        $strText = $blurbs['name_1000258'];
+	        
+	        // Bild
+	        $strImageUrl = $blurbs['name_1000260']['url'];
+	        $strImageAlt = $blurbs['name_1000260']['alt'];
+	        $strImageWidth = $blurbs['name_1000260']['width'];
+	        $strImageHeight = $blurbs['name_1000260']['height'];
+	        
+	        // Pusha tillbaka till arrayen $myData
+	        array_push($myData, array(
+	           'link_url' => $strLinkUrl,
+	           'link_title' => $strLinkTitle,
+	           'link_target' => $strLinkTarget,
+	           'text_content' => $strText,
+	           'image_url' => $strImageUrl,
+	           'image_alt' => $strImageAlt,
+	           'image_width' => $strImageWidth,
+	           'image_height' => $strImageHeight
+	        ));
+        }
+        
+        // Returnera länkar
+		return $myData;
+	}
+
+	// Metod som anropas när pluginen aktiveras
+	function region_halland_acf_front_page_blurbs_activate() {
+		// Ingenting just nu...
+	}
+
+	// Metod som anropas när pluginen av-aktiveras
+	function region_halland_acf_front_page_blurbs_deactivate() {
+		// Ingenting just nu...
+	}
+
+	// Aktivera pluginen och anropa metod
+	register_activation_hook( __FILE__, 'region_halland_acf_front_page_blurbs_activate');
+
+	// Av-aktivera pluginen och anropa metod
+	register_deactivation_hook( __FILE__, 'region_halland_acf_front_page_blurbs_deactivate');
+
+?>
